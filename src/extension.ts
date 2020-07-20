69e96c1d776e8f5506ca3d5efae7158459d08507
sanggeon <sssang97@svcorps.dev>
2020-07-20 09:08:11 +0900

    0.1.2

--- src/extension.ts
+++ src/extension.ts
@@ -5,18 +5,11 @@ import * as basic_provider from "./provider/basic_provider";
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-    let disposable = vscode.commands.registerCommand(
-        "swagger-jsdoc.helloWorld",
-        () => {
-            // The code you place here will be executed every time your command is executed
+    let trigger = vscode.commands.registerCommand("swagger-jsdoc", () => {
+        vscode.window.showInformationMessage("trigger swagger-jsdoc");
+    });
 
-            // Display a message box to the user
-            vscode.window.showInformationMessage("trigger swagger-jsdoc");
-        }
-    );
-
-    context.subscriptions.push(disposable);
-    context.subscriptions.push(basic_provider.provider);
+    context.subscriptions.push(trigger, basic_provider.provider);
 }
 
 // this method is called when your extension is deactivated
